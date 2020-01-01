79bf603f78b3ec096b38cd8615114247f7b08f2d
Rizwan Javed <rizwan.javed@purelogics.net>
2020-01-01 12:17:44 +0500

    Created Databse Table using Database Migration

--- database/migrations/2020_01_01_071047_create_students_table.php
+++ database/migrations/2020_01_01_071047_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,14 @@ class CreatePasswordResetsTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-    }
+{
+    Schema::create('students', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('name');
+        $table->string('course');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('students');
     }
 }
